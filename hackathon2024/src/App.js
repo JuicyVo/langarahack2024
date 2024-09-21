fbc1f8829f05d2bbc22e4cf73d1150afe6597fc4
EnriqueSaracho <90583844+EnriqueSaracho@users.noreply.github.com>
2024-09-21 11:00:46 -0700

    first kiki commit

--- hackathon2024/src/App.js
+++ hackathon2024/src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Langara Hackathon 2024
-        </p>
-       
+        <p>Langara Hackathon 2024</p>
+        <p>Kiki Update</p>
       </header>
     </div>
   );
